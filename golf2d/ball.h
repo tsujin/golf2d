d27cb5f023e3f42e1547fab7557ca2d732da943f
Justin <contact@tsujin.com>
2021-09-28 21:49:10 -0400

    added 2d vector for computing ball position and velocity

--- golf2d/ball.h
+++ golf2d/ball.h
@@ -1,15 +1,14 @@
 #pragma once
 #include <SDL.h>
 
-struct Vec2f {
-	float x, y;
-};
+struct Vec2f { float x, y; };
 
 class Ball {
 	SDL_Texture* sprite;
 
 public:
 	Vec2f position, velocity;
+	Ball();
 	float operator +(Vec2f& other);
 	void update();
 };
