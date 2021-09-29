bdba06573c227bf6b6d494bedc34353ba87c06d7
Justin <contact@tsujin.com>
2021-09-28 21:48:47 -0400

    added deltaTime

--- golf2d/main.cpp
+++ golf2d/main.cpp
@@ -1,14 +1,23 @@
 #include "game.h"
+#include <iostream>
 
 int main(int argc, char* argv[]) {
 	Game game;
 	game.start("Golf2d");
+	Uint64 NOW = SDL_GetPerformanceCounter();
+	Uint64 LAST = 0;
+	double deltaTime = 0;
 
 	while (game.running) {
+		LAST = NOW;
+		NOW = SDL_GetPerformanceCounter();
+
+		deltaTime = (double)((NOW - LAST) * 1000 / (double)SDL_GetPerformanceFrequency());
 		game.HandleEvents();
 		game.update();
 		game.render();
 
+		std::cout << deltaTime << "\n";
 	}
 	
 	game.cleanup();
